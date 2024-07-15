442408233e6596fc542ce070c5fad17f861f4289
MichaelBanyard <michaelbanyard2004@gmail.com>
2024-07-15 10:32:15 -0700

    Just experimenting

--- Project/src/main/java/main/CityApplication.java
+++ Project/src/main/java/main/CityApplication.java
@@ -15,12 +15,16 @@ public class CityApplication extends Application {
         stage.setTitle("Hello!");
         stage.setWidth(500);
         stage.setHeight(500);
-        Button b = new Button("Click me!");
+        Button b = new Button("Button of DOOM!");
         b.setOnMouseClicked((action -> {
             if(action.getButton() == MouseButton.PRIMARY) {
                 System.out.println("Hello!");
             }
         }));
+        root.setOnMouseMoved(event -> {
+            b.setTranslateX(event.getX() - root.getWidth() / 2);
+            b.setTranslateY(event.getY() - root.getHeight() / 2);
+        });
         stage.setScene(scene);
         root.getChildren().addAll(b);
         stage.show();
